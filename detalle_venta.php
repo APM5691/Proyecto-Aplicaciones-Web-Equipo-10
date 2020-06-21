29bd0f14ad473fc5b897e14d2673781958be2487
Miguel Herrera <al221911721@gmail.com>
2020-06-20 19:12:58 -0500

    se agragaron 2 apartados de php

---
+++ detalle_venta.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Marcas</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/all.min.css" />
+</head>
+
+<body>
+    <?php readfile('./menu.html'); ?>
+
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-header">
+                Marca
+            </div>
+            <div class="card-body">
+                <table class="table table-dark table table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th style="width: 90%;" scope="col">Marca</th>
+                            <th style="width: 10%;" scope="col">
+                                <a class="btn btn-outline-primary" href="marcas_formulario.html"><i
+                                        class="fa fa-plus"></i></a>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Luminosete</th>
+                            <td>
+                                <a class="btn btn-primary"><i class="fas fa-plus-square"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
