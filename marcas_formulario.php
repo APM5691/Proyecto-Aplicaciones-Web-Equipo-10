2e5b6c43942f48a49699c2832dee5e009da6a4c8
Angel Palacios Mirafuentes <al221910938@gmail.com>
2020-06-23 17:02:45 -0500

    se termino mi parte(creo)

--- marcas_formulario.php
+++ marcas_formulario.php
@@ -18,17 +18,49 @@
         Marcas
         <a type="button" class="btn btn-outline-dark float-right" href="marcas.php">Regresar</a>
       </div>
+
       <div class="card-body">
         <form action="marcas_guardar.php" method="post">
           <div class="form-group">
-            <label for="marca">Nombre</label>
+            <label for="marca">Nombre de la marca</label>
             <input type="text" class="form-control form-control-sm" id="marca" name="marca"
               aria-describedby="marca_help" />
-            <small id="marca_help" class="form-text text-muted">Escribe el nombre del producto</small>
+            <small id="marca_help" class="form-text text-muted">Escribe el nombre de la marca</small>
           </div>
+
+          <div class="form-group">
+            <label for="direccion_marca">Direccion</label>
+            <input type="text" class="form-control form-control-sm" id="direccion_marca" name="direccion_marca"
+              aria-describedby="direccion_help" />
+            <small id="direccion_help" class="form-text text-muted">Escribe la direccion de la marca</small>
+          </div>
+
+          <div class="form-group">
+            <label for="pais">Pais</label>
+            <input type="text" class="form-control form-control-sm" id="pais" name="pais"
+              aria-describedby="pais_help" />
+            <small id="pais_help" class="form-text text-muted">Escribe pais de donde es originaria la marca</small>
+          </div>
+
+          <div class="form-group">
+            <label for="correo_electronico_marca">Correo electronico</label>
+            <input type="text" class="form-control form-control-sm" id="correo_electronico_marca" name="correo_electronico_marca"
+              aria-describedby=correo_electronico_marca_help" />
+            <small id="correo_electronico_marca_help" class="form-text text-muted">Escribe el nombre del producto</small>
+          </div>
+
+          <div class="form-group">
+            <label for="telefono">Telefono</label>
+            <input type="text" class="form-control form-control-sm" id="telefono" name="telefono"
+              aria-describedby="telefono_help" />
+            <small id="telefono_help" class="form-text text-muted">Escribe el telefono de la marca</small>
+          </div>
+          
+
           <button class="btn btn-success btn-sm" type="submit">
             <i class="fa fa-save"></i> Guardar
           </button>
+
         </form>
       </div>
     </div>
--- marcas_guardar.php
+++ marcas_guardar.php
@@ -20,6 +20,50 @@
         ?>
     </pre>
     </div>
+    <div class="container">
+    <pre>
+    <?php
+        if (!isset($_POST['direccion_marca']) || empty($_POST['direccion_marca'])) {
+            echo '<strong>Debes escribir un valor para "direccion_marca"</strong>';
+        } else {
+            echo '<strong>El valor que escribiste para direccion_marca es: ' . $_POST['direccion_marca'] . '</strong>';
+        }
+        ?>
+    </pre>
+    </div>
+    <div class="container">
+    <pre>
+    <?php
+        if (!isset($_POST['pais']) || empty($_POST['pais'])) {
+            echo '<strong>Debes escribir un valor para "pais"</strong>';
+        } else {
+            echo '<strong>El valor que escribiste para pais es: ' . $_POST['pais'] . '</strong>';
+        }
+        ?>
+    </pre>
+    </div>
+    <div class="container">
+    <pre>
+    <?php
+        if (!isset($_POST['correo_electronico_marca']) || empty($_POST['correo_electronico_marca'])) {
+            echo '<strong>Debes escribir un valor para "correo_electronico_marca"</strong>';
+        } else {
+            echo '<strong>El valor que escribiste para correo_electronico_marca es: ' . $_POST['correo_electronico_marca'] . '</strong>';
+        }
+        ?>
+    </pre>
+    </div>
+    <div class="container">
+    <pre>
+    <?php
+        if (!isset($_POST['telefono']) || empty($_POST['telefono'])) {
+            echo '<strong>Debes escribir un valor para "telefono"</strong>';
+        } else {
+            echo '<strong>El valor que escribiste para telefono es: ' . $_POST['telefono'] . '</strong>';
+        }
+        ?>
+    </pre>
+    </div>
     <script src="js/jquery-3.5.1.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
