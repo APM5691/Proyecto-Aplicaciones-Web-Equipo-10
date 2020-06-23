4cd70b7da69598d9ff43157e0594a5dedee12f44
Miguel Herrera <al221911721@gmail.com>
2020-06-23 15:02:31 -0500

    actualizando el menu

--- productos_formulario.php
+++ productos_formulario.php
@@ -11,47 +11,47 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="Inicio.html"><i class="fa fa-gem"></i> Venta de joyas</a>
+        <a class="navbar-brand" href="Inicio.php"><i class="fa fa-gem"></i> Venta de joyas</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="Inicio.html"><i class="fa fa-home"></i> Inicio<span
-                            class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Catalogo</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="usuarios_formulario.html"><i class="fa fa-address-card"></i>Ingresar</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Opciones
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="ventas_formulario.html"> Ventas</a>
-                        <a class="dropdown-item" href="marcas_formulario.html">Marcas</a>
-                        <a class="dropdown-item" href="Usuarios.html">Usuarios</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="Productos.php">Productos</a>
-                        <a class="dropdown-item" href="tipo_de_joya_formulario.php">Tipo de Joya</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-eye"></i>Ver</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-            </form>
-        </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="Inicio.php"><i class="fa fa-home"></i> Inicio<span
+                        class="sr-only">(current)</span></a>
+            </li>
+            <!--<li class="nav-item">
+                <a class="nav-link" href="#">Catalogo</a>
+            </li>-->
+           
+            <li class="nav-item">
+                <a class="nav-link" href="clientes_formularios.php"><i class="fa fa-address-card"></i>Ingresar</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Opciones
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="ventas_formulario.php"> Ventas</a>
+                    <a class="dropdown-item" href="marcas_formulario.php">Marcas</a>
+                    <a class="dropdown-item" href="Usuarios.php">Usuarios</a>
+                    <a class="dropdown-item" href="Productos.php">Productos</a>
+                    <a class="dropdown-item" href="clientes_formularios.php">Clientes</a>
+                    <a class="dropdown-item" href="tipo_de_joya_formulario.php">Tipo de Joya</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-eye"></i>Ver</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+         </form>
+    </div>
     </nav>
     <div class="container mt-4">
         <div class="card">
@@ -74,14 +74,16 @@
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="marca">Nombre</label>
-                        <input type="text" class="form-control form-control-sm" id="marca" name="marca"
-                            aria-describedby="marca_help">
-                        <small id="marca_help" class="form-text text-muted">Escribe el nombre del producto</small>
+                        <label for="nombre_producto">Nombre</label>
+                        <input type="text" class="form-control form-control-sm" id="nombre_producto" name="nombre_producto"
+                            aria-describedby="nombre_producto_help">
+                        <small id="nombre_producto_help" class="form-text text-muted">Escribe el nombre del producto</small>
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <label for="precio">Precio</label>
+                        <input type="number" class="form-control form-control-sm" id="precio" name="precio"
+                            aria-describedby="precio_help">
+                        <small id="precio_help" class="form-text text-muted">Escribe el precio del producto</small>
                     </div>
                     <button class="btn btn-success btn-sm" type="submit"> <i class="fa fa-save"></i> Guardar</button>
                 </form>
--- tipo_de_joya_formulario.php
+++ tipo_de_joya_formulario.php
@@ -10,49 +10,50 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="Inicio.html"><i class="fa fa-gem"></i> Venta de joyas</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="Inicio.php"><i class="fa fa-gem"></i> Venta de joyas</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="Inicio.html"><i class="fa fa-home"></i> Inicio<span
-                            class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Catalogo</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="usuarios_formulario.html"><i class="fa fa-address-card"></i>Ingresar</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Opciones
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="ventas_formulario.html"> Ventas</a>
-                        <a class="dropdown-item" href="marcas_formulario.html">Marcas</a>
-                        <a class="dropdown-item" href="Usuarios.html">Usuarios</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="Productos.html">Productos</a>
-                        <a class="dropdown-item" href="tipo_de_joya_formulario.html">Tipo de Joya</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-eye"></i>Ver</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-            </form>
-        </div>
-    </nav>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="Inicio.php"><i class="fa fa-home"></i> Inicio<span
+                        class="sr-only">(current)</span></a>
+            </li>
+            <!--<li class="nav-item">
+                <a class="nav-link" href="#">Catalogo</a>
+            </li>-->
+           
+            <li class="nav-item">
+                <a class="nav-link" href="clientes_formularios.php"><i class="fa fa-address-card"></i>Ingresar</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Opciones
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="ventas_formulario.php"> Ventas</a>
+                    <a class="dropdown-item" href="marcas_formulario.php">Marcas</a>
+                    <a class="dropdown-item" href="Usuarios.php">Usuarios</a>
+                    <a class="dropdown-item" href="Productos.php">Productos</a>
+                    <a class="dropdown-item" href="clientes_formularios.php">Clientes</a>
+                    <a class="dropdown-item" href="tipo_de_joya_formulario.php">Tipo de Joya</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-eye"></i>Ver</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>
+    </div>
+</nav>
     <div class="container mt-4">
         <div class="card">
             <div class="card-header">
