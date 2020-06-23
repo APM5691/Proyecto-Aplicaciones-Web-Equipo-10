3c614403119b0cf6328ac74ec52aa0f4f07f0ce3
Miguel Herrera <al221911721@gmail.com>
2020-06-22 20:48:23 -0500

    agregando codigo

--- materiales_formulario.php
+++ materiales_formulario.php
@@ -25,8 +25,26 @@
                         <small id="id_help" class="form-text text-muted">Escribe el id</small>127
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <label for="material_principal">Material Principal</label>
+                        <select class="form-control" id="material_principal" name="material_principal">
+                            <option>Seleccionar</option>
+                            <option>Oro</option>
+                            <option>Plata</option>
+                            <option>Metal Bañado en Oro</option>
+                            <option>Metal Bañado en Plata</option>
+                        </select>
+                        <small id="material_principal_help" class="form-text text-muted">Selecciona el Material Principal</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="material_secundario">Material Secundario</label>
+                        <select class="form-control" id="material_secundario" name="material_secundario">
+                            <option>Seleccionar</option>
+                            <option>Oro</option>
+                            <option>Plata</option>
+                            <option>Metal Bañado en Oro</option>
+                            <option>Metal Bañado en Plata</option>
+                        </select>
+                        <small id="material_secundario_help" class="form-text text-muted">Selecciona el Material Secundario</small>
                     </div>
                     <button class="btn btn-success btn-sm" type="submit"> <i class="fa fa-save"></i> Guardar</button>
                 </form>
