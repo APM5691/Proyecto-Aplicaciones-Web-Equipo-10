6b0e8c5e2570bba2690020851792bfe3156a7f53
Miguel Herrera <al221911721@gmail.com>
2020-06-24 00:03:51 -0500

    cambiando vinculo materiales

--- Productos.php
+++ Productos.php
@@ -51,13 +51,10 @@
                         </tr>
                     </tbody>
                 </table>
+                <a href="materiales.php" class="btn btn-success float-right"> Materiales</a>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-    <a href="materiales.php" class="btn btn-success float-right"> Siguiente</a>
-=======
->>>>>>> f795a5d4885a058b4cfbce6e73fa883e9fd372ca
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
