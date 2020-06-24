9eec9d227dd55eb75cfc37acbb12a9368faa051f
Miguel Herrera <al221911721@gmail.com>
2020-06-24 00:07:32 -0500

    actualizando el codigo

--- Productos.php
+++ Productos.php
@@ -55,6 +55,36 @@
             </div>
         </div>
     </div>
+    <div>
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-header">
+                Materiales
+                <a href="productos_formulario.php"class="btn btn-success float-right">Agregar</a>
+            </div>
+            <div class="card-body">
+                <table class="table table-dark table table-hover">
+                    <thead class="thead-light">
+                        <tr>
+                            <th style="width: 45%;" scope="col">Id del producto</th>
+                            <th style="width: 45%;" scope="col">Material</th>
+                            <th style="width: 90%;" scope="col">
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">127</th>
+                            <th scope="row">Oro</th>
+                            <td>
+                                <a class="btn btn-primary"><i class="fas fa-plus-square"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
