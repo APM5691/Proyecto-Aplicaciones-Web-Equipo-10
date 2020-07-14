8057dd8d09e5aff29b48642969638207aeb8821a
Angel Palacios Mirafuentes <al221910938@gmail.com>
2020-07-13 21:30:10 -0500

    Conexion a base de datos

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+require('./vendor/autoload.php');
+$dotenv = Dotenv\Dotenv::createImmutable('./');
+$dotenv->load();
+
+try {
+    $conexion = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';charset=utf8;port=' . $_ENV['DB_PORT'] . ';dbname=' . $_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+    // set the PDO error mode to exception
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Conexion establecida';
+    
+} catch(PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
+?>
