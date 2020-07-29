5d754d747d4e781c33602453d1b0d8f6392806aa
Josue Manjarrez Careaga <al221910471@gmail.com>
2020-07-28 22:40:56 -0500

    Avance en update ventas

---
+++ ventas_actualizar.php
@@ -0,0 +1,28 @@
+<?php
+if (
+    !isset($_POST['id_venta']) || !is_numeric($_POST['id_venta'])
+    || !isset($_POST['monto_total']) || empty($_POST['monto_total'])
+    || !isset($_POST['direcciones_id']) || empty($_POST['direcciones_id'])
+    || !isset($_POST['clientes_id1']) || empty($_POST['clientes_id1'])
+) {
+    header('Location: ventas_formulario.php?info=Parámetros incorrectos');
+    exit;
+}
+
+require_once './conexion.php';
+$sql = <<<fin
+update venta set
+    monto_total = :monto_total
+    , direcciones_id = :direcciones_id
+    , clientes_id1 = :clientes_id1
+where
+    id_venta = :id_venta
+fin;
+$sentencia = $conexion->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+$sentencia->execute([
+    ':monto_total' => $_POST['monto_total']
+    , ':direcciones_id' => $_POST['direcciones_id']
+    , ':clientes_id1' => $_POST['clientes_id1']
+]);
+header('Location: ventas.php?info=Usuario actualizado exitosamente');
+?>
