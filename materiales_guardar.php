c5e386d482b7ed04dac3a01610aa00fa018358b6
Miguel Herrera <al221911721@gmail.com>
2020-08-02 12:52:06 -0500

    modificando codigo

---
+++ materiales_guardar.php
@@ -0,0 +1,24 @@
+<?php
+if (
+    !isset($_POST['nombre']) || empty($_POST['nombre'])
+    || !isset($_POST['tipo_material']) || empty($_POST['tipo_material'])
+) {
+    header('Location: materiales_formulario.php?info=Parámetros incorrectos');
+
+    exit;
+}
+
+require_once './conexion.php';
+$sql = <<<fin
+insert into material set
+    nombre = :nombre
+    , tipo_material = :tipo_material
+fin;
+
+$sentencia = $conexion->prepare($sql, [PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY]);
+$sentencia->execute([
+    ':nombre' => $_POST['nombre']
+    , ':tipo_material' => $_POST['tipo_material']
+]);
+header('Location: materiales.php?info=Material creado exitosamente');
+?>
